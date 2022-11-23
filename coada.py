9c297e86bdedc7fdafe327630ef5aa991ca4a954
radumsk <raducostache@mail.ru>
2022-11-24 01:14:21 +0200

    Am reusit sa fac bot-ul random in fisierul coada.py si sa leg procese din doua fisire diferite

--- coada.py
+++ coada.py
@@ -1,12 +1,28 @@
 import pygame
 import sys
 from inter import *
-from main import *
+#from main import *
 from multiprocessing import Process, Queue
 
-mainqueue = Queue()
-mainqueue.put("TARIE")
-process_guessword = Process(target=check_guess,args=guess_to_check)
+def pickRandom(guessQueue):
+    n = 0
+    while True:
+        if guessQueue.empty:
+            guessQueue.put(WORDS[random.randrange(len(WORDS))])
+            n = n + 1
+        if n == 10:
+            break
+if __name__ == '__main__':
+    guessQueue = Queue()
+    #getWord(guessQueue)
+    process_guessword = Process(target=getWord,args=(guessQueue,))
+    process_randomword = Process(target=pickRandom, args=(guessQueue,))
+    process_guessword.start()
+    process_randomword.start()
+    process_guessword.join()
+    process_randomword.join()
 
-process_guessword.start()
-process_guessword.join()
+
+#    process_guessword = Process(target=getWord,args=(guessQueue,))
+#   process_guessword.start()
+#    process_guessword.join()
--- inter.py
+++ inter.py
@@ -213,25 +213,8 @@ def getWord(guessQueue):
                         break
                 if nextWord == 1:
                     break
-def pickRandom(guessQueue):
-    n = 0
-    while True:
-        if guessQueue.empty:
-            guessQueue.put(WORDS[random.randrange(len(WORDS))])
-            n = n + 1
-        if n == 10:
-            break
 
 
-if __name__ == '__main__':
-    guessQueue = Queue()
-    #getWord(guessQueue)
-    process_guessword = Process(target=getWord,args=(guessQueue,))
-    process_randomword = Process(target=pickRandom, args=(guessQueue,))
-    process_guessword.start()
-    process_randomword.start()
-    process_guessword.join()
-    process_randomword.join()
 '''    if game_result != "":
         play_again()
     for event in pygame.event.get():
